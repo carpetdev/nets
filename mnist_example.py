b082aef8a6e3f05daecc822ab972e975d59234f5
Ibraheem Sajid <ibraheem.sajid@gmail.com>
2020-06-22 07:04:09 +0100

    It trains!
    Add comments on the DataLoader process.

--- mnist_example.py
+++ mnist_example.py
@@ -3,17 +3,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.datasets.mnist as mnist
+import torch.utils.data as D
+import torch.optim as optim
+import torchvision.transforms as T
+from torchvision.datasets import MNIST
 
 class Net(nn.Module):
 
     def __init__(self):
         super().__init__()
-        # Suppose input is single channel, 28x28 pixels, padded to 32x32
+        # Suppose input is single channel, 32x32 pixels
         # 6 feature maps, 28x28 pixels, max-pooled to 14x14
         # then 16 feature maps, 12x12 pixels, max-pooled to 6x6, reshaped to a 16*6*6 row vector
         # followed by 3 fully connected layers 16*6*6 -> 120 -> 84 -> 10
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 3)
         self.fc1 = nn.Linear(16*6*6, 120)
         self.fc2 = nn.Linear(120, 84)
@@ -27,6 +30,33 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return F.softmax(x)
+        return x.softmax(dim=1)
+
+    def train(self):
+        batch_size = 4
+        transform = T.Compose([T.Resize(32), T.ToTensor(), T.Normalize(0.5, 0.5)])
+        train_data = MNIST('./assets', train=True, download=True, transform=transform)
+        train_loader = D.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.1)
+
+        epochs = 1
+        for epoch in range(epochs):
+            for i, data in enumerate(train_loader):
+                inputs, labels = data
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                labels = torch.zeros(batch_size, 10).scatter(1, labels.reshape(batch_size,1), 1)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                if i % 2000 == 1999:
+                    print(f"[{epoch+1:d}, {i+1:5d}] loss = {loss.item()}")
+                    print(outputs)
+                    print(labels)
+
+
 
 net = Net()
+net.train()
